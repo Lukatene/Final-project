93ac0efeaecd563de8f763a8f1d0296c40aded0c
Lukatene <99635692+Lukatene@users.noreply.github.com>
2022-04-20 20:11:51 +0400

    Update skills.js

--- js/skills.js
+++ js/skills.js
@@ -1,12 +1,16 @@
 
 {
-class TextFX {
+    class TextFX {
         constructor(el) {
             this.DOM = {el: el};
+     
             this.DOM.texts = [...this.DOM.el.querySelectorAll('.content__text')];
             this.DOM.textsTotal = this.DOM.texts.length;
+
             this.middleIdx = Math.floor(this.DOM.textsTotal/2);
+
             this.loopInterval = {show: 80, hide: 80};
+
             this.loopEndIddleTime = this.loopInterval.show;
         }
         show({dir = 'both', halfwayCallback = null} = {}) {
@@ -93,11 +97,11 @@ class TextFX {
 
             this.textFX = new TextFX(this.DOM.el.querySelector('.content__text-wrap'));
         }
-
+      
         hideImage(dir) {
             this.toggleImage('hide', dir);
         }
-
+       
         showImage(dir) {
             this.toggleImage('show', dir);
         }
@@ -119,25 +123,25 @@ class TextFX {
     class Slideshow {
         constructor(el) {
             this.DOM = {el: el};
-
+           
             this.DOM.nav = {
                 prev: this.DOM.el.querySelector('.content__nav-button--prev'),
                 next: this.DOM.el.querySelector('.content__nav-button--next')
             };
-
+           
             this.slides = [];
             [...this.DOM.el.querySelectorAll('.content__slide')].forEach(slide => this.slides.push(new Slide(slide)));
-
+           
             this.slidesTotal = this.slides.length;
-
+            
             this.current = 0;
-
+           
             this.slides[this.current].DOM.el.classList.add('content__slide--current');
-
+          
             this.initEvents();
         }
         initEvents() {
-
+          
             this.onClickPrevFn = _ => this.navigate('prev');
             this.onClickNextFn = _ => this.navigate('next');
             this.DOM.nav.prev.addEventListener('click', this.onClickPrevFn);
@@ -148,38 +152,40 @@ class TextFX {
                 return false;
             }
             this.isAnimating = true;
-
+            
             const currentSlide = this.slides[this.current];
             
-
+           
             this.current = dir === 'next' ? 
                 this.current < this.slidesTotal - 1 ? this.current + 1 : 0 :
                 this.current > 0 ? this.current - 1 : this.slidesTotal - 1;
-
+                
+            
             const upcomingSlide = this.slides[this.current];
 
             const onCurrentHalfwayCallback = () => {
-
+             
                 currentSlide.hideImage(dir);
-
+              
                 upcomingSlide.textFX.DOM.texts[upcomingSlide.textFX.middleIdx].style.opacity = 0;
-
+                (opacity = 1)
                 upcomingSlide.DOM.el.classList.add('content__slide--current');
-
+                
                 upcomingSlide.showImage(dir);
             };
             const onCurrentEndCallback = () => {
-
+         
                 currentSlide.DOM.el.classList.remove('content__slide--current');
                 upcomingSlide.textFX.show().then(() => this.isAnimating = false);
             };
-
+    
             currentSlide.textFX.hide({halfwayCallback: onCurrentHalfwayCallback}).then(onCurrentEndCallback);
         }
     }
 
-
+ 
     new Slideshow(document.querySelector('.content'));
 
+  
     imagesLoaded(document.querySelectorAll('.content__img-inner'), {background: true}, () => document.body.classList.remove('loading'));
 }
