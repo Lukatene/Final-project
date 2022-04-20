352563bbd0f2f778ada28043b4f5811c8e99ff51
Lukatene <99635692+Lukatene@users.noreply.github.com>
2022-04-20 20:06:00 +0400

    Update skills.js

--- js/skills.js
+++ js/skills.js
@@ -1,13 +1,14 @@
-
-{
-    class TextFX {
+ class TextFX {
         constructor(el) {
             this.DOM = {el: el};
-           
+            // The texts (repeated)
             this.DOM.texts = [...this.DOM.el.querySelectorAll('.content__text')];
-            this.DOM.textsTotal = this.DOM.texts.length;           
-            this.middleIdx = Math.floor(this.DOM.textsTotal/2);         
+            this.DOM.textsTotal = this.DOM.texts.length;
+            // The index of the main text element
+            this.middleIdx = Math.floor(this.DOM.textsTotal/2);
+            // Time between each showing/hiding of each text instance
             this.loopInterval = {show: 80, hide: 80};
+            // optional: Extra time to the whole show/hide animation. 
             this.loopEndIddleTime = this.loopInterval.show;
         }
         show({dir = 'both', halfwayCallback = null} = {}) {
@@ -91,11 +92,14 @@
                 wrap: this.DOM.el.querySelector('.content__img'),
                 inner: this.DOM.el.querySelector('.content__img-inner')
             };
+            // The text effect class.
             this.textFX = new TextFX(this.DOM.el.querySelector('.content__text-wrap'));
         }
+        // Hide the Slide's image
         hideImage(dir) {
             this.toggleImage('hide', dir);
         }
+        // Show the Slide's image
         showImage(dir) {
             this.toggleImage('show', dir);
         }
@@ -117,20 +121,25 @@
     class Slideshow {
         constructor(el) {
             this.DOM = {el: el};
-
+            // Navigation controls
             this.DOM.nav = {
                 prev: this.DOM.el.querySelector('.content__nav-button--prev'),
                 next: this.DOM.el.querySelector('.content__nav-button--next')
             };
-
+            // All slides
             this.slides = [];
             [...this.DOM.el.querySelectorAll('.content__slide')].forEach(slide => this.slides.push(new Slide(slide)));
+            // Total number of slides
             this.slidesTotal = this.slides.length;
+            // Current slide position
             this.current = 0;
+            // Show the first one
             this.slides[this.current].DOM.el.classList.add('content__slide--current');
+            // Initialize some events
             this.initEvents();
         }
         initEvents() {
+            // Clicking next and previous controls.
             this.onClickPrevFn = _ => this.navigate('prev');
             this.onClickNextFn = _ => this.navigate('next');
             this.DOM.nav.prev.addEventListener('click', this.onClickPrevFn);
@@ -141,31 +150,42 @@
                 return false;
             }
             this.isAnimating = true;
+            // Current slide
             const currentSlide = this.slides[this.current];
             
-
+            // Update current
             this.current = dir === 'next' ? 
                 this.current < this.slidesTotal - 1 ? this.current + 1 : 0 :
                 this.current > 0 ? this.current - 1 : this.slidesTotal - 1;
                 
+            // Upcoming slide
             const upcomingSlide = this.slides[this.current];
 
             const onCurrentHalfwayCallback = () => {
+                // Hide the current slide's image
                 currentSlide.hideImage(dir);
+                // Set the upcoming slide's main text opacity to 1.
                 upcomingSlide.textFX.DOM.texts[upcomingSlide.textFX.middleIdx].style.opacity = 0;
+                // Add current class to the upcoming slide (opacity = 1)
                 upcomingSlide.DOM.el.classList.add('content__slide--current');
+                // Show the upcoming slide's image
                 upcomingSlide.showImage(dir);
             };
             const onCurrentEndCallback = () => {
+                // Remove the current class from the current slide.
                 currentSlide.DOM.el.classList.remove('content__slide--current');
                 upcomingSlide.textFX.show().then(() => this.isAnimating = false);
             };
-        
+            // Hide the current slide's text, 
+            // and call onCurrentHalfwayCallback at half of the animation
+            // In the end call onCurrentEndCallback
             currentSlide.textFX.hide({halfwayCallback: onCurrentHalfwayCallback}).then(onCurrentEndCallback);
         }
     }
 
-
+    // Initialize the slideshow
     new Slideshow(document.querySelector('.content'));
+
+    // Preload all the images in the page
     imagesLoaded(document.querySelectorAll('.content__img-inner'), {background: true}, () => document.body.classList.remove('loading'));
 }
